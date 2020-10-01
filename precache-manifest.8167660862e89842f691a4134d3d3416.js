87f9ca619effae3dea73e30df0d768edea050bd9
Malachi-Egbugha <malachi.egbugha3@gmail.com>
2020-10-01 13:24:40 +0100

    Updates

---
+++ precache-manifest.8167660862e89842f691a4134d3d3416.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1901d450709a9f8a0cdf96d2fb85b5c",
+    "url": "/elearningclient/index.html"
+  },
+  {
+    "revision": "ffc0bbef4e277b9b877b",
+    "url": "/elearningclient/static/css/main.be14dcc8.chunk.css"
+  },
+  {
+    "revision": "9b887f83fa424609ed7c",
+    "url": "/elearningclient/static/js/2.e82ba67e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/elearningclient/static/js/2.e82ba67e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc0bbef4e277b9b877b",
+    "url": "/elearningclient/static/js/main.bf08cde5.chunk.js"
+  },
+  {
+    "revision": "d1acdf88a90f395a5e68",
+    "url": "/elearningclient/static/js/runtime-main.32193ea5.js"
+  },
+  {
+    "revision": "5c2db8b1013319f2eca1d4b39bab911a",
+    "url": "/elearningclient/static/media/afrilearn_logo.5c2db8b1.png"
+  },
+  {
+    "revision": "7146862611465d56e1fbf72a6bfc5347",
+    "url": "/elearningclient/static/media/banner.71468626.jpg"
+  },
+  {
+    "revision": "f7d8bfcc27f3769ff3595d02cb577079",
+    "url": "/elearningclient/static/media/jamb.f7d8bfcc.png"
+  },
+  {
+    "revision": "7156e57a7117f322af818badbbf1c30c",
+    "url": "/elearningclient/static/media/necco.7156e57a.png"
+  },
+  {
+    "revision": "1e5ea518198e8ec38f1341f164932e0a",
+    "url": "/elearningclient/static/media/professional.1e5ea518.webp"
+  },
+  {
+    "revision": "1f3aff80b2007ccb2af64fe4a2461d15",
+    "url": "/elearningclient/static/media/waec.1f3aff80.png"
+  }
+]);
